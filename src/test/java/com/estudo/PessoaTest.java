a7f1d141603d8517e84a71643de60b178ec0df51
AlexRitter1 <ritter.alex@hotmail.com>
2025-07-10 11:54:31 -0300

    boas práticas

--- src/test/java/com/estudo/PessoaTest.java
+++ src/test/java/com/estudo/PessoaTest.java
@@ -8,8 +8,20 @@ import java.time.LocalDate;
 public class PessoaTest {
 
     @Test
-    void getIdadeTest() {
+    void deveCalcularIdade() {
         Pessoa alex = new Pessoa("Alex", LocalDate.of(2000, 7, 20));
         Assertions.assertEquals(24, alex.getIdade());
+    }
+
+    @Test
+    void deveVerificarIdadeMaiorQue18(){
+        Pessoa alex = new Pessoa("Alex", LocalDate.of(2000, 7, 20));
+        Assertions.assertTrue(alex.ehMaiorDeIdade());
+    }
+
+    @Test
+    void deveVerificarIdadeMenorQue18(){
+        Pessoa alex = new Pessoa("Alex", LocalDate.of(2015, 7, 20));
+        Assertions.assertFalse(alex.ehMaiorDeIdade());
     }
 }
